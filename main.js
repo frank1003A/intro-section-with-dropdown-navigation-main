8e69d5a4a31729d626f4bc85c69d2d71da206a65
Frank Ezene <63779386+frank1003A@users.noreply.github.com>
2023-05-24 10:41:24 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,76 @@
+"use strict";
+let nav = document.querySelector("nav");
+let img = document.getElementById("hero-img");
+let openBtn = document.getElementById("hamburger-open");
+let closeBtn = document.getElementById("hamburger-close");
+let drawer = document.getElementById("drawer");
+let overlay = document.getElementById("overlay");
+
+let dropdown = document.getElementsByClassName("custom-dropdown");
+
+const unmountElement = (el) => {
+  el.style.display = "none";
+};
+
+unmountElement(overlay)
+//drawer.classList.add("drawer-close");
+
+for (let i = 0; i < nav.children.length; i++) {
+  let links = nav.children;
+  let isSelect = links[i].className === "custom-select";
+
+  if (isSelect) {
+    // Tags
+    let btn = document.getElementById(links[i].children[0].id);
+    let opt = document.getElementById(btn.nextElementSibling.id);
+    // Remove options
+    unmountElement(opt);
+    // Button Event Listeners
+    btn.addEventListener("mouseenter", function () {
+      opt.style.display = "";
+    });
+
+    btn.addEventListener("mouseleave", function () {
+      opt.style.display = "none";
+    });
+
+    // Options Event Listeners
+    opt.addEventListener("mouseenter", function () {
+      opt.style.display = "";
+    });
+    opt.addEventListener("mouseleave", function () {
+      opt.style.display = "none";
+    });
+  }
+}
+
+const drawerOpen = () => {
+  drawer.classList.remove("drawer-close");
+  drawer.classList.add("drawer-open");
+  overlay.style.display = "";
+};
+
+const drawerClose = () => {
+  drawer.classList.remove("drawer-open");
+  drawer.classList.add("drawer-close");
+  overlay.style.display = "none";
+};
+
+openBtn.addEventListener("click", () => drawerOpen());
+closeBtn.addEventListener("click", () => drawerClose());
+overlay.addEventListener("click", () => drawerClose());
+
+// DRAWER DROPDOWN
+for (let i = 0; i < dropdown.length; i++) {
+  let dropdownBtn = document.getElementById(dropdown[i].children[0].id);
+  let dpCont = document.getElementById(dropdown[i].children[1].id);
+  let isOpen = false;
+  dropdownBtn.addEventListener("click", function () {
+    isOpen = !isOpen;
+    if (isOpen) {
+      dpCont.style.height = "120px";
+    } else {
+      dpCont.style.height = "0px";
+    }
+  });
+}
